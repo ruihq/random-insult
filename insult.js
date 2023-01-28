c0e245338e5b034548448ea7c6cca8ad435a18dd
Lunime ૮ ˶ᵔ ᵕ ᵔ˶ ა <120704613+ruihq@users.noreply.github.com>
2023-01-28 19:43:32 +0800

    Update insult.js

--- insult.js
+++ insult.js
@@ -1,1 +1,9 @@
-
+fetch("insults.json")
+  .then(response => response.json())
+  .then(data => {
+    insults = data.insults;
+    generateBtn.addEventListener("click", () => {
+      const randomIndex = Math.floor(Math.random() * insults.length);
+      insult.textContent = insults[randomIndex];
+    });
+  });
